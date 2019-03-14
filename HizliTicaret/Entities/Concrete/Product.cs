1a9106aadcb17640c6bccec5c31d1621bda84dea
Ersoy TORAMAN <jovannijares@gmail.com>
2019-03-14 20:02:21 +0300

    Resim, resim yolu olarak güncellendi.

--- HizliTicaret/Entities/Concrete/Product.cs
+++ HizliTicaret/Entities/Concrete/Product.cs
@@ -13,6 +13,6 @@ namespace Entities.Concrete
         public string Name { get; set; }
         public string Description { get; set; }
         public int Stock { get; set; }
-        public byte[] Image { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
