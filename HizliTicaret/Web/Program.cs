c2db7b42331b62f7c5bd874115b7f9561aca625c
Ersoy TORAMAN <jovannijares@gmail.com>
2019-03-18 20:34:49 +0300

    layoutlar eklendi.

--- HizliTicaret/Web/Program.cs
+++ HizliTicaret/Web/Program.cs
@@ -17,8 +17,6 @@ namespace Web
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
     }
 }
