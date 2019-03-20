e3a484d13c2dbe83210cf37911564d7d92aee19e
Ersoy TORAMAN <jovannijares@gmail.com>
2019-03-20 19:16:53 +0300

    UserNav view component eklendi.

--- HizliTicaret/Web/ViewComponents/UserNav.cs
+++ HizliTicaret/Web/ViewComponents/UserNav.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Web.ViewComponents
 {
     public class UserNav : ViewComponent
     {
-
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
     }
 }
