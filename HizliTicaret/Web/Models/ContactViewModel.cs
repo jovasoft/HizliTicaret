5b6dde3e0c82b6896ff1439385028cc5fbe6a81c
Ersoy TORAMAN <jovannijares@gmail.com>
2019-04-04 01:49:57 +0300

    iletisim sayfası tamamlandı.

---
+++ HizliTicaret/Web/Models/ContactViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Mail { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
